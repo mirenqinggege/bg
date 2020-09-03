d578a62ea9675aa3867fa9a7ac597c66b7563541
oyghan <742163033@qq.com>
2020-09-03 17:30:55 +0800

    1.添加文件上传工具类

--- src/main/java/com/fumei/bg/util/FileUploadUtil.java
+++ src/main/java/com/fumei/bg/util/FileUploadUtil.java
@@ -1,7 +1,18 @@
 package com.fumei.bg.util;
 
+import com.fumei.bg.domain.File;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  * @author zkh
  */
 public class FileUploadUtil {
+
+    public static File fileUpload(MultipartFile uploadFile){
+        if(uploadFile == null || uploadFile.isEmpty()){
+            return null;
+        }
+        File file = new File();
+        return file;
+    }
 }
