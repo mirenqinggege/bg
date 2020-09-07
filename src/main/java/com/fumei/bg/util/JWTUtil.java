1a91ad16ea97b6eeb7fc41b848472612497128ad
oyghan <742163033@qq.com>
2020-09-07 09:41:11 +0800

    JWTUtil 签名完成

--- src/main/java/com/fumei/bg/util/JWTUtil.java
+++ src/main/java/com/fumei/bg/util/JWTUtil.java
@@ -1,9 +1,11 @@
 package com.fumei.bg.util;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fumei.bg.config.Global;
 
 import java.util.Date;
+import java.util.HashMap;
 
 /**
  * @author zkh
@@ -14,9 +16,16 @@ public class JWTUtil {
     private static final long EXPIRE_TIME = Long.parseLong(Global.getConfig("Token.expireTime"));
     private static final String TOKEN_SECRET = Global.getConfig("Token.secret");
 
-    public static String sign(Long userId){
+    public static String sign(Long userId) {
         Date date = new Date(System.currentTimeMillis() + EXPIRE_TIME);
         Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
-        return null;
+        HashMap<String, Object> map = new HashMap<>(2);
+        map.put("Type", "Jwt");
+        map.put("alg", "HS256");
+        return JWT.create()
+                .withHeader(map)
+                .withClaim("userId", userId)
+                .withExpiresAt(date)
+                .sign(algorithm);
     }
 }
