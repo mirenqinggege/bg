40434237228270d6cf16d6e6525ae419c49ce8b8
oyghan <742163033@qq.com>
2020-09-07 15:18:46 +0800

    JsonUtil   json 工具类 LoginStatusValidateInterceptor  登录状态验证拦截器

---
+++ src/main/java/com/fumei/bg/interceptor/LoginStatusValidateInterceptor.java
@@ -0,0 +1,35 @@
+package com.fumei.bg.interceptor;
+
+import com.fumei.bg.util.JWTUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author zkh
+ */
+@Component
+public class LoginStatusValidateInterceptor implements HandlerInterceptor {
+    private final static Logger log = LoggerFactory.getLogger(LoginStatusValidateInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/json");
+        return JWTUtil.validateToken(request, response);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
